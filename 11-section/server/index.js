b18b13fe3cb6a5a5b7b3e7f8b0c8cc138944b2de
aglebionek <aglebionek@gmail.com>
2024-10-14 00:01:34 +0200

    add error handling to server

--- 11-section/server/index.js
+++ 11-section/server/index.js
@@ -41,29 +41,44 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
+  try {
+    const values = await pgClient.query('SELECT * from values');
 
-  res.send(values.rows);
+    res.send(values.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(`Error: ${err}`);
+  }
 });
 
 app.get('/values/current', async (req, res) => {
-  redisClient.hgetall('values', (err, values) => {
-    res.send(values);
-  });
+  try {
+    redisClient.hgetall('values', (err, values) => {
+      res.send(values);
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(`Error: ${err}`);
+  }
 });
 
 app.post('/values', async (req, res) => {
-  const index = req.body.index;
+  try {
+    const index = req.body.index;
 
-  if (parseInt(index) > 40) {
-    return res.status(422).send('Index too high');
+    if (parseInt(index) > 40) {
+      return res.status(422).send('Index too high');
+    }
+
+    redisClient.hset('values', index, 'Nothing yet!');
+    redisPublisher.publish('insert', index);
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+    res.send({ working: true });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(`Error: ${err}`);
   }
-
-  redisClient.hset('values', index, 'Nothing yet!');
-  redisPublisher.publish('insert', index);
-  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
-
-  res.send({ working: true });
 });
 
 app.listen(5000, (err) => {
